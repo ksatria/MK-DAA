eff856a04155647d88ede9b953b0e43af9dbc91f
Khoirul Umam <khoirul.umam35@gmail.com>
2017-08-06 22:35:57 +0700

    Add Standard Fibonacci Programs

---
+++ Code/StandardFibonacci/StandardFibonacci/Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardFibonacci
+{
+    class Fibonacci
+    {
+        public int get(int n)
+        {
+            if (n == 0) return 0;
+            else if (n == 1) return 1;
+            else return get(n - 1) + get(n - 2);
+        }
+    }
+}
---
+++ Code/StandardFibonacci/StandardFibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandardFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, result;
+            Fibonacci fibo = new Fibonacci();
+
+            Console.Write("Request bilangan Fibonacci ke : ");
+            n = Int32.Parse(Console.ReadLine());
+
+            result = fibo.get(n);
+
+            Console.WriteLine("Bilangan Fibonacci ke-{0} adalah {1}", n, result);
+            Console.Read();
+        }
+    }
+}

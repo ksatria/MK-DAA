140f535b1ed35ae1d031a17752bfba672fd4f0a9
Khoirul Umam <khoirul.umam35@gmail.com>
2017-08-07 09:13:38 +0700

    Refinement Code Standard Fibonacci

--- Code/StandardFibonacci/StandardFibonacci/Fibonacci.cs
+++ Code/StandardFibonacci/StandardFibonacci/Fibonacci.cs
@@ -10,9 +10,8 @@ namespace StandardFibonacci
     {
         public double get(int n)
         {
-            if (n == 0)         return 0;
-            else if (n == 1)    return 1;
-            else                return get(n - 1) + get(n - 2);
+            if (n <= 1)	return n;
+            else		return get(n - 1) + get(n - 2);
         }
     }
 }

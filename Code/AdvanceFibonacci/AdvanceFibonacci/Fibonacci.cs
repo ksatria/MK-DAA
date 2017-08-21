ff73baeb8d1d42b318f64ef6355f22c8175f262c
Khoirul Umam <khoirul.umam35@gmail.com>
2017-08-21 08:08:28 +0700

    Add Advance Fibonacci Codes

---
+++ Code/AdvanceFibonacci/AdvanceFibonacci/Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceFibonacci
+{
+    class Fibonacci
+    {
+        public double get(int n)
+        {
+            double o = (1 + Math.Sqrt(5)) / 2;
+
+            return Math.Round(1 / Math.Sqrt(5) * Math.Pow(o, n));
+        }
+    }
+}
---
+++ Code/AdvanceFibonacci/AdvanceFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double result;
+            Fibonacci fibo = new Fibonacci();
+
+            Console.Write("Request bilangan Fibonacci ke : ");
+            n = Int32.Parse(Console.ReadLine());
+
+            result = fibo.get(n);
+
+            Console.WriteLine("Bilangan Fibonacci ke-{0} adalah {1}", n, result);
+            Console.Read();
+        }
+    }
+}

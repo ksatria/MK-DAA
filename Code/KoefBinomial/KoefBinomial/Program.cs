5204d14e868dad367567d3aa2ad81ee70ab96846
Khoirul Umam <khoirul.umam35@gmail.com>
2017-10-23 14:59:56 +0700

    Contoh Kode Dynamic Programming

---
+++ Code/KoefBinomial/KoefBinomial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoefBinomial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k, hasil;
+            Binomial bin = new Binomial();
+
+            Console.Write("Masukkan indeks baris : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan indeks kolom : ");
+            k = Convert.ToInt32(Console.ReadLine());
+
+            hasil = bin.koefisien(n, k);
+
+            Console.WriteLine("Koefisien binomial ({0},{1}) adalah {2}", n, k, hasil);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Code/Persekutuan/Persekutuan/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persekutuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FPB f = new FPB(19293800, 12680);
+            Console.WriteLine("Hasil = {0}", f.run());
+            Console.ReadLine();
+        }
+    }
+}
